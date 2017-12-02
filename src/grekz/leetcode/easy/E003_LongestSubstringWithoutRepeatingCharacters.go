2162544f697ac1e475adb271d5c8d459a2978992
grekz <vidbla.grekz@gmail.com>
2017-12-03 00:41:29 +0100

    GLCE-003 - Longest Substring Without Repeating Characters - Solution.

---
+++ src/grekz/leetcode/easy/E003_LongestSubstringWithoutRepeatingCharacters.go
@@ -0,0 +1,19 @@
+package easy
+
+func lengthOfLongestSubstring(s string) int {
+    res, prev := 0, 0
+    dic := make(map[rune]int)
+    for i, x := range s {
+        if v, ok := dic[x]; ok && v >= prev {
+            if d := i - prev; d > res {
+                res = d
+            }
+            prev = v + 1
+        }
+        dic[x] = i
+    }
+    if r := len(s) - prev; r > res {
+        return r
+    }
+    return res
+}
