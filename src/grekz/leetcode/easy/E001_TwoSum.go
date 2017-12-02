7205ba37bd34772868466dbbb70143fd670e23e7
grekz <vidbla.grekz@gmail.com>
2017-12-02 23:05:40 +0100

    GLCE-001 - Two Sum - Solution.

---
+++ src/grekz/leetcode/easy/E001_TwoSum.go
@@ -0,0 +1,17 @@
+package easy
+
+import (
+	"fmt"
+)
+
+func twoSum(nums []int, target int) []int {
+    tmp := make(map[int]int)
+    for i, v := range nums {
+        if x, ok := tmp[target - v]; ok {
+            return []int{x, i}
+        }
+        tmp[v] = i
+        i++
+    }		
+    return []int{0,0}
+}
