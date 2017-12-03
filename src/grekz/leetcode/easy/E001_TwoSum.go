73976d7b4dc11b7ac9cdac1afbdae3df9288c74a
grekz <vidbla.grekz@gmail.com>
2017-12-03 12:40:59 +0100

    Moving solutions to proper package, plus format coding.

--- src/grekz/leetcode/easy/E001_TwoSum.go
+++ src/grekz/leetcode/easy/E001_TwoSum.go
@@ -1,13 +1,13 @@
 package easy
 
 func twoSum(nums []int, target int) []int {
-    tmp := make(map[int]int)
-    for i, v := range nums {
-        if x, ok := tmp[target - v]; ok {
-            return []int{x, i}
-        }
-        tmp[v] = i
-        i++
-    }		
-    return []int{0,0}
+	tmp := make(map[int]int)
+	for i, v := range nums {
+		if x, ok := tmp[target-v]; ok {
+			return []int{x, i}
+		}
+		tmp[v] = i
+		i++
+	}
+	return []int{0, 0}
 }
