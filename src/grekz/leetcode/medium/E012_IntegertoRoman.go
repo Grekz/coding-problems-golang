c2d4d1b9bb98a87d06e9f3f09ea8350766f142a8
grekz <vidbla.grekz@gmail.com>
2017-12-11 20:54:58 +0100

    GLCE-012 - Integer to Roman - Solution.

---
+++ src/grekz/leetcode/medium/E012_IntegertoRoman.go
@@ -0,0 +1,11 @@
+package medium
+
+func intToRoman(n int) string {
+    r := [...]string {
+            "","I","II","III","IV","V","VI","VII","VIII","IX",
+            "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+            "","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+            "","M","MM","MMM","MMMM",
+    }
+    return r[n/1000 + 30] + r[(n%1000)/100 + 20] + r[(n%100)/10 + 10] + r[n%10]    
+}
