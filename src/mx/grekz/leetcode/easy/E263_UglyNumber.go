5342c41b4ea48dee31ada0914d61aa8ffadbfd61
grekz <vidbla.grekz@gmail.com>
2018-03-18 22:35:37 +0100

    GLCE-263. Ugly Number

---
+++ src/mx/grekz/leetcode/easy/E263_UglyNumber.go
@@ -0,0 +1,11 @@
+package easy
+
+// @author grekz
+func isUgly(num int) bool {
+    if (num == 0 || num == 14) {return false;}
+    if (num == 1 || num == 6 || num == 8) {return true;}
+    if ( num % 5 == 0) {return isUgly(num / 5);}
+    if ( num % 3 == 0) {return isUgly(num / 3);}
+    if ( num % 2 == 0) {return isUgly(num / 2);}
+    return false;
+}
