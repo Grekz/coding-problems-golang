5b737e834618e156940d94bb13abca3215153b1b
Grekz <vidbla.grekz@gmail.com>
2018-06-18 23:23:58 +0200

    GLCE-162. Find Peak Element

---
+++ src/mx/grekz/leetcode/medium/E162_FindPeakElement.go
@@ -0,0 +1,15 @@
+package medium
+
+// @author grekz
+func findPeakElement(nums []int) int {
+    l, h := 0, len(nums) - 1
+    for l < h {
+        m := ( l + h ) / 2
+        if nums[m] < nums[m+1] {
+            l = m + 1
+        }else{
+            h = m
+        }
+    }
+    return l
+}
