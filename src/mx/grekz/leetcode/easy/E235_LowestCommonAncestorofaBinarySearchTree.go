338efc5f2f1eebb4602b9d303082a8a2db0dddb0
Grekz <vidbla.grekz@gmail.com>
2019-02-03 02:29:15 +0100

    GLCE-235. Lowest Common Ancestor of a Binary Search Tree

---
+++ src/mx/grekz/leetcode/easy/E235_LowestCommonAncestorofaBinarySearchTree.go
@@ -0,0 +1,16 @@
+package easy
+
+// @author grekz
+/**
+ * Definition for TreeNode.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *ListNode
+ *     Right *ListNode
+ * }
+ */
+ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    if ( root.Val > p.Val && root.Val > q.Val ) { return lowestCommonAncestor(root.Left, p, q) }
+    if ( root.Val < p.Val && root.Val < q.Val ) { return lowestCommonAncestor(root.Right, p, q) }
+    return root
+}
