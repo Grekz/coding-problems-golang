2bd75daa2d787844d3acdb40ff65cea24fcf2e2d
Grekz <vidbla.grekz@gmail.com>
2019-02-20 12:49:45 +0100

    GLCE-672. Bulb Switcher II

---
+++ src/mx/grekz/leetcode/medium/E672_BulbSwitcherII.go
@@ -0,0 +1,12 @@
+package medium
+
+// @author grekz
+func flipLights(n int, m int) int {
+    if(m == 0) {return 1}
+    if(n == 1) {return 2}
+    if(n == 2 && m == 1) {return 3}
+    if(n == 2 || m == 1) {return 4}
+    if(m == 2) {return 7}
+    if(m >= 3) {return 8}
+    return 8
+}
