224e40ea0be040e70235d4bb51421a1404208a08
Grekz <vidbla.grekz@gmail.com>
2019-02-26 23:19:42 +0100

    GLCE-343. Integer Break

---
+++ src/mx/grekz/leetcode/medium/E343_IntegerBreak.go
@@ -0,0 +1,14 @@
+package medium
+
+// @author grekz
+func integerBreak(n int) int {
+    if n == 2 { return 1 }
+    if n == 3 { return 2 }
+    res := 1
+    for n > 4 { 
+        res *= 3
+        n -= 3
+    }
+    res *= n
+    return res
+}
