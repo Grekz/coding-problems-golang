a2158e7c68859cb28b4fd1da1d57226f8a2534dd
Grekz <vidbla.grekz@gmail.com>
2019-03-05 22:26:06 +0100

    GLCE-142. Linked List Cycle II

--- src/mx/grekz/leetcode/medium/E142_LinkedListCycleII.go
+++ src/mx/grekz/leetcode/medium/E142_LinkedListCycleII.go
@@ -1,3 +1,26 @@
 package medium
 
 // @author grekz
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func detectCycle(head *ListNode) *ListNode {
+    slow, fast := head, head
+    for fast != nil && fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+        if ( slow == fast ) {
+            slow = head
+            for slow != fast {
+                slow = slow.Next
+                fast = fast.Next
+            }
+            return slow
+        }
+    }
+    return nil
+}
